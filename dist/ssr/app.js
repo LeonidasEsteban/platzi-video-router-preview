4251a3a5141ee7af553d12ca07e15e1babd4cea1
Leonidas Esteban <leonidasesteban@gmail.com>
2018-08-08 02:22:55 -0500

    [add] withRouter support

--- dist/ssr/app.js
+++ dist/ssr/app.js
@@ -14045,6 +14045,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
@@ -14057,70 +14059,112 @@ var _logo = __webpack_require__(94);
 
 var _logo2 = _interopRequireDefault(_logo);
 
+var _reactRouter = __webpack_require__(137);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Header = function Header() {
-  return _react2.default.createElement(
-    'header',
-    { className: 'Header' },
-    _react2.default.createElement('img', { src: _logo2.default, width: 250 }),
-    _react2.default.createElement(
-      'nav',
-      null,
-      _react2.default.createElement(
-        'ul',
-        null,
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Header = function (_Component) {
+  _inherits(Header, _Component);
+
+  function Header() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, Header);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Header.__proto__ || Object.getPrototypeOf(Header)).call.apply(_ref, [this].concat(args))), _this), _this.handleClick = function () {
+      _this.props.history.goBack();
+    }, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  _createClass(Header, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'header',
+        { className: 'Header' },
+        _react2.default.createElement('img', { src: _logo2.default, width: 250 }),
         _react2.default.createElement(
-          'li',
+          'nav',
           null,
           _react2.default.createElement(
-            _reactRouterDom.NavLink,
-            { exact: true, to: '/', activeClassName: 'is-selected' },
-            'Inicio'
-          )
-        ),
-        _react2.default.createElement(
-          'li',
-          null,
-          _react2.default.createElement(
-            _reactRouterDom.NavLink,
-            { to: '/videos', activeClassName: 'is-selected' },
-            'Videos'
-          )
-        ),
-        _react2.default.createElement(
-          'li',
-          null,
-          _react2.default.createElement(
-            _reactRouterDom.NavLink,
-            { to: '/v', activeClassName: 'is-selected' },
-            'Redirect'
-          )
-        ),
-        _react2.default.createElement(
-          'li',
-          null,
-          _react2.default.createElement(
-            _reactRouterDom.NavLink,
-            { to: '/contacto', activeClassName: 'is-selected' },
-            'Contacto'
-          )
-        ),
-        _react2.default.createElement(
-          'li',
-          null,
-          _react2.default.createElement(
-            _reactRouterDom.NavLink,
-            { to: '/perfil', activeClassName: 'is-selected' },
-            'Perfil'
+            'ul',
+            null,
+            _react2.default.createElement(
+              'li',
+              null,
+              _react2.default.createElement(
+                _reactRouterDom.NavLink,
+                { exact: true, to: '/', activeClassName: 'is-selected' },
+                'Inicio'
+              )
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              _react2.default.createElement(
+                _reactRouterDom.NavLink,
+                { to: '/videos', activeClassName: 'is-selected' },
+                'Videos'
+              )
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              _react2.default.createElement(
+                _reactRouterDom.NavLink,
+                { to: '/v', activeClassName: 'is-selected' },
+                'Redirect'
+              )
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              _react2.default.createElement(
+                _reactRouterDom.NavLink,
+                { to: '/contacto', activeClassName: 'is-selected' },
+                'Contacto'
+              )
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              _react2.default.createElement(
+                _reactRouterDom.NavLink,
+                { to: '/perfil', activeClassName: 'is-selected' },
+                'Perfil'
+              )
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              _react2.default.createElement(
+                'a',
+                { onClick: this.handleClick },
+                '\uD83D\uDC48'
+              )
+            )
           )
         )
-      )
-    )
-  );
-};
+      );
+    }
+  }]);
 
-exports.default = Header;
+  return Header;
+}(_react.Component);
+
+exports.default = (0, _reactRouter.withRouter)(Header);
 
 /***/ }),
 /* 93 */
--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,42 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './header.css';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../../images/logo.png';
+import { withRouter } from 'react-router';
 
-const Header = () => (
-  <header className="Header">
-    <img src={logo} width={250}/>
+class Header extends Component {
+  handleClick = () => {
+    this.props.history.goBack();
+  }
+  render() {
+    return (
+      <header className="Header">
+        <img src={logo} width={250}/>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="is-selected">
+                Inicio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/videos" activeClassName="is-selected">
+                Videos
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/v" activeClassName="is-selected">
+                Redirect
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacto" activeClassName="is-selected">
+                Contacto
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/perfil" activeClassName="is-selected">
+                Perfil
+              </NavLink>
+            </li>
+            <li>
+              <a onClick={this.handleClick}>👈</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
 
-    <nav>
-      <ul>
-        <li>
-          <NavLink exact to="/" activeClassName="is-selected">
-            Inicio
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/videos" activeClassName="is-selected">
-            Videos
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/v" activeClassName="is-selected">
-            Redirect
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contacto" activeClassName="is-selected">
-            Contacto
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/perfil" activeClassName="is-selected">
-            Perfil
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
-
-export default Header
+export default withRouter(Header)
